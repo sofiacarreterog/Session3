3bc4d19ac32175084d634b800aa345654a4046ed
sofiacarreteroguerrero <sofia.carretero.g@gmail.com>
2025-01-14 18:42:35 +0100

    We carried out arithmetic operations: adding, subtract etc

---
+++ arithmetic operators.py
@@ -0,0 +1,8 @@
+print(2+3) #addition
+print(2-3) #substraction
+print(2*3) #multiplication
+print(4/2) #division - floating number
+print(type(4/2), 4/2)
+print(4//2) #integer division
+print(9%5) #modulo - the remainder of a division (9/5 = 1 remainder 2)
+print(2**3) #power
